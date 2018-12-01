49656a67d55ebc642cf7e02535c36ea294693139
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2018-12-01 22:07:17 +0530

    Dynamic component - Implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ComponentFactory,
+  ComponentRef,
+  ComponentFactoryResolver,
+  ViewContainerRef,
+  ViewChild,
+  OnDestroy
+} from '@angular/core';
+import { AlertComponent } from './alert.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'first-dynamic-component';
+
+  @ViewChild('alertContainer', { read: ViewContainerRef }) container;
+  componentRef: ComponentRef<any>;
+
+  constructor(private resolver: ComponentFactoryResolver) { }
+
+  createComponent(type) {
+    this.container.clear();
+    const factory: ComponentFactory<any> = this.resolver.resolveComponentFactory(
+      AlertComponent
+    );
+
+    this.componentRef = this.container.createComponent(factory);
+
+    this.componentRef.instance.type = type;
+
+    this.componentRef.instance.output.subscribe(event => console.log(event));
+  }
+
+  ngOnDestroy() {
+    this.componentRef.destroy();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AlertComponent } from './alert.component';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, AlertComponent],
+  entryComponents: [AlertComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
